1f5a4f9a9e6c84015737864ad75e5ef75a9b100b
Killian <killian@hp3822e2ad3bc3.home>
2021-08-04 12:38:21 +0200

    Index change

--- index.js
+++ index.js
@@ -6,22 +6,21 @@ fetch('http://localhost:3000/api/cameras') // Requête http //
         let getAllCameras = ""; // Variable pour les produits //
 
         for (let i in response) { // Boucle de récupération //
-            console.log(response[i].name); // Vérification de la récupération //
 
         getAllCameras += // Création HTML des produits //
             `<div class="card col-md-5 mb-lg-4 card__body">
                 <a href="./product.html?${response[i]._id}" class="product-link">
                     <img class="card-img-top card__img" src="${response[i].imageUrl}" alt="Appareil photo vintage" />
                 </a>
-                    <div class="card-body card__body-text">
-                        <h2 class="card-title mt-1">
-                            ${response[i].name}
-                        </h2>
-                        <p class="card-text text-right font-weight-bold">
-                            ${(response[i].price/100).toFixed(2).replace(".",",")}€
-                        </p>
-                    </div>
-                </div>`
+                <div class="card-body card__body-text">
+                    <h2 class="card-title mt-1">
+                        ${response[i].name}
+                    </h2>
+                    <p class="card-text text-right font-weight-bold">
+                        ${(response[i].price/100).toFixed(2).replace(".",",")}€
+                    </p>
+                </div>
+            </div>`
         }
 
         document.getElementById("camera-cards").innerHTML = getAllCameras; // Insertion des produits //
