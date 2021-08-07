e286a8bb78a21cf82a88e7d76cf54905cca68b34
Killian <killian@hp3822e2ad3bc3.home>
2021-08-07 09:26:53 +0200

    Add total price display

--- js/basket.js
+++ js/basket.js
@@ -20,20 +20,20 @@ else{ // Si il y a un article dans le panier injecter le produit dans la page //
     for(j = 0; j < productRegisteredInLocalStorage.length; j++){ // Boucle de récupération du tableau //
 
         inTheBasket = inTheBasket + // Code html du produit //
-            `<div class="row mt-3 mb-3">
-                <div class="col-3 text-center">
+            `<div class="row mt-3 mb-3 pb-3 border-bottom">
+                <div class="col-12 col-md-3 text-center">
                     <!-- Insertion de la photo du produit -->
                     <img src="${productRegisteredInLocalStorage[j].image}" class="panier__img" alt="Appareil photo vintage" />
                 </div>
-                <div class="col-3 text-center mt-5">
+                <div class="col-6 col-md-3 text-left text-md-center mt-5">
                     <!-- Insertion de la désignation du produit -->
                     ${productRegisteredInLocalStorage[j].name}
                 </div>
-                <div class="col-3 text-center mt-5">
+                <div class="col-6 col-md-3 text-right text-md-center mt-5">
                     <!-- Insertion de l'option du produit -->
                     ${productRegisteredInLocalStorage[j].lense}
                 </div>
-                <div class="col-3 text-center mt-5">
+                <div class="col-12 col-md-3 text-right text-md-center mt-5">
                     <!-- Insertion du prix produit -->
                     ${(productRegisteredInLocalStorage[j].price/100).toFixed(2).replace(".",",")} €
                 </div>
@@ -66,3 +66,19 @@ else{ // Si il y a un article dans le panier injecter le produit dans la page //
 
     })
 }
+
+// ---------- Prix total ---------- //
+
+let totalPriceTable = []; // Tableau de regroupement du prix des produits // 
+
+for(let k = 0; k < productRegisteredInLocalStorage.length; k++){ // Boucle de récupération des prix //
+    priceProductsInTheBasket = productRegisteredInLocalStorage[k].price; // Récupération du prix des produits dans le local storage //
+
+    totalPriceTable.push(priceProductsInTheBasket) // Envoie du prix des articles dans le tableau //
+}
+
+const reducer = (accumulator, currentValue) => accumulator + currentValue; // Constante pour le calcul du prix //
+
+const totalPrice = totalPriceTable.reduce(reducer,0); // Envoie du calcul dans le total //
+
+document.getElementById('price-display').innerHTML = totalPrice;
